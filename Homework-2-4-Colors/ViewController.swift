ca17ea85134d97e27ca6446966821c966ac16037
Aleksey Zhurkin <web.zhurkin@gmail.com>
2024-11-29 10:24:25 +0300

    Initial Commit

---
+++ Homework-2-4-Colors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-2-4-Colors
+//
+//  Created by Алексей Журкин on 29.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
